8deda9864ad616c7ca8c10f6abd65aaf412b9c53
kaimen1202 <72474050+kaimen1202@users.noreply.github.com>
2020-10-21 19:17:50 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,6 +1,7 @@
 from discord.ext import commands
 import os
 import traceback
+import random
 
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
@@ -12,5 +13,10 @@ async def on_command_error(ctx, error):
     error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
     await ctx.send(error_msg)
 
+@client.event
+async def on_message(message):
+    if "h.goroku" in message.content:
+        word_list = ["ブンブンハローRed♪Tube","SexkinTV♪every day♪","ひき逃げです","ヒカキンさんのアナ、ゥお借りしたいんです！","OnakinTV♪every day♪","ナイチンチンゲール♪","ウンコの力","ケツの穴が無い！","パンパンパンパンパン！パンパン！","メッサムラムラ！？","ローション！？","最初はブンブンじゃんけん出たー！出た！出たぁ...","あ～ん！ちゃんち～ん！あっちっち～","なんか4545ってこれ誰だ？遊んでんのかな？"]
+        await message.channel.send(random.choice(word_list))
 
 bot.run(token)
