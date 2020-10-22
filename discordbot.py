deed4a701efaf9d1855dd8a61a0d4d72d9046046
kaimen1202 <72474050+kaimen1202@users.noreply.github.com>
2020-10-22 10:15:33 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -16,7 +16,7 @@ async def on_command_error(ctx, error):
 
 @client.event
 async def on_message(message):
-    if "h.goroku" in message.content:
+    if "!語録" in message.content:
         word_list = ["ブンブンハローRed♪Tube","SexkinTV♪every day♪","ひき逃げです","ヒカキンさんのアナ、ゥお借りしたいんです！","OnakinTV♪every day♪","ナイチンチンゲール♪","ウンコの力","ケツの穴が無い！","パンパンパンパンパン！パンパン！","メッサムラムラ！？","ローション！？","最初はブンブンじゃんけん出たー！出た！出たぁ...","あ～ん！ちゃんち～ん！あっちっち～","なんか4545ってこれ誰だ？遊んでんのかな？"]
         await message.channel.send(random.choice(word_list))
 
